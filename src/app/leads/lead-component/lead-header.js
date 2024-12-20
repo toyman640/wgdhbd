6c502bc75a857a1f09d9c8f2ae901a6c103a95a3
toyman640 <falakoadebayor@gmail.com>
2024-12-20 17:07:25 +0100

    Make header responsive

--- src/app/leads/lead-component/lead-header.js
+++ src/app/leads/lead-component/lead-header.js
@@ -1,63 +1,77 @@
-import { PresentationChartLineIcon } from "@heroicons/react/24/outline";
-import { ListBulletIcon, PlusIcon, TrashIcon, ChevronDownIcon, EllipsisVerticalIcon, Bars3BottomLeftIcon, PencilSquareIcon, 
-ArrowTopRightOnSquareIcon, ArrowPathIcon, UserGroupIcon, ChartPieIcon  } from "@heroicons/react/24/outline"
-
+import {
+  PresentationChartLineIcon,
+  ListBulletIcon,
+  PlusIcon,
+  TrashIcon,
+  ChevronDownIcon,
+  EllipsisVerticalIcon,
+  Bars3BottomLeftIcon,
+  PencilSquareIcon,
+  ArrowTopRightOnSquareIcon,
+  ArrowPathIcon,
+  UserGroupIcon,
+  ChartPieIcon,
+} from "@heroicons/react/24/outline";
 
 export default function LeadHeader() {
   return (
-    <div className="flex justify-between shadow-xl rounded p-1 font-sans text-sm">
-      <div className="flex w-1/3">
-        <p className="">My open leads</p><ChevronDownIcon class="h-4 w-4 mt-1 text-gray-500 mt-1" />
+    <div className="flex flex-col lg:flex-row lg:justify-between shadow-xl rounded p-3 font-sans text-sm">
+      {/* Left Section */}
+      <div className="flex items-center mb-3 lg:mb-0">
+        <p className="mr-2">My open leads</p>
+        <ChevronDownIcon className="h-4 w-4 text-gray-500" />
       </div>
-      <div className="flex justify-between w-2/3">
-        <button className="flex">
-          <PresentationChartLineIcon className="h-4 w-4 mt-1 mr-1 text-gray-500" />
+
+      {/* Right Section */}
+      <div className="flex flex-wrap lg:flex-nowrap items-center justify-start lg:justify-end gap-2 overflow-x-auto">
+        <button className="flex items-center whitespace-nowrap">
+          <PresentationChartLineIcon className="h-4 w-4 mr-1 text-gray-500" />
           <p>Show chart</p>
         </button>
-        <button className="flex">
-          <ListBulletIcon className="h-4 w-4 mt-1 mr-1 text-gray-500" />
+        <button className="flex items-center whitespace-nowrap">
+          <ListBulletIcon className="h-4 w-4 mr-1 text-gray-500" />
           <p>Focused view</p>
         </button>
-        <button className="flex">
-          <PlusIcon className="h-4 w-4 mt-1 mr-1 text-gray-500" />
+        <button className="flex items-center whitespace-nowrap">
+          <PlusIcon className="h-4 w-4 mr-1 text-gray-500" />
           <p>New</p>
         </button>
-        <button className="flex">
-          <ArrowPathIcon className="h-4 w-4 mt-1 mr-1 text-gray-500" />
+        <button className="flex items-center whitespace-nowrap">
+          <ArrowPathIcon className="h-4 w-4 mr-1 text-gray-500" />
           <p>Refresh</p>
         </button>
-        <button className="flex">
-          <UserGroupIcon className="h-4 w-4 mt-1 mr-1 text-gray-500" />
-          <p>Collaborate</p>  
+        <button className="flex items-center whitespace-nowrap">
+          <UserGroupIcon className="h-4 w-4 mr-1 text-gray-500" />
+          <p>Collaborate</p>
         </button>
-        <button className="flex">
-          <TrashIcon className="h-4 w-4 mt-1 mr-1 text-gray-500" />
+        <button className="flex items-center whitespace-nowrap">
+          <TrashIcon className="h-4 w-4 mr-1 text-gray-500" />
           <p>Delete</p>
         </button>
-        <div className="flex">
-          <p>|</p>
-          <ChevronDownIcon className="h-4 w-4 mt-1 mr-1 text-gray-500" />
-          <EllipsisVerticalIcon className="h-4 w-4 mt-1 mr-1 text-gray-500" />
+        <div className="flex items-center whitespace-nowrap">
+          <p className="mr-2">|</p>
+          <ChevronDownIcon className="h-4 w-4 mr-2 text-gray-500" />
+          <EllipsisVerticalIcon className="h-4 w-4 text-gray-500" />
         </div>
-        <button className="flex border-solid border rounded py-1 px-2 border-gray-300">
-          <ChartPieIcon className="h-4 w-4 mt-1 mr-1 text-gray-500" /> Smart data
+        <button className="flex items-center whitespace-nowrap border border-gray-300 rounded py-1 px-2">
+          <ChartPieIcon className="h-4 w-4 mr-1 text-gray-500" />
+          Smart data
         </button>
-        <button className="flex border-solid border rounded py-1 px-2 border-gray-300">
-          <Bars3BottomLeftIcon className="h-4 w-4 mt-1 mr-1 text-gray-500" /> Edit filters
+        <button className="flex items-center whitespace-nowrap border border-gray-300 rounded py-1 px-2">
+          <Bars3BottomLeftIcon className="h-4 w-4 mr-1 text-gray-500" />
+          Edit filters
         </button>
-        <button className="flex border-solid border rounded py-1 px-2 border-gray-300">
-          <PencilSquareIcon className="h-4 w-4 mt-1 mr-1 text-gray-500" /> Edit columns
+        <button className="flex items-center whitespace-nowrap border border-gray-300 rounded py-1 px-2">
+          <PencilSquareIcon className="h-4 w-4 mr-1 text-gray-500" />
+          Edit columns
         </button>
-        <div className="bg-blue-700 rounded p-1">
-
-          <button className="border-r border-white-500">
-            <ArrowTopRightOnSquareIcon className="h-4 w-4 mt-1 mr-1 text-white" />
-            
+        <div className="flex bg-blue-700 rounded p-1 items-center">
+          <button className="border-r border-white-500 pr-2">
+            <ArrowTopRightOnSquareIcon className="h-4 w-4 text-white" />
           </button>
-          <button className="">
-
-            <ChevronDownIcon className="h-4 w-4 mt-1 mr-1 text-white" />
-          </button> 
+          <button className="pl-2">
+            <ChevronDownIcon className="h-4 w-4 text-white" />
+          </button>
         </div>
       </div>
     </div>
