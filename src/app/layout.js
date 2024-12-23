f2fe8ec05dd883fa692244f6d881b248d53500f5
toyman640 <falakoadebayor@gmail.com>
2024-12-23 21:01:25 +0100

    Fix sidebar design

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { MinusIcon, TvIcon, HomeIcon, UserIcon, CogIcon, ClipboardDocumentListIcon, Bars3Icon  } from '@heroicons/react/24/outline';
+import { MinusIcon, TvIcon, HomeIcon, UserIcon, CogIcon, ClipboardDocumentListIcon, Bars3Icon, ClockIcon  } from '@heroicons/react/24/outline';
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "./components/Navbar";
 import Link from 'next/link';
@@ -35,8 +35,8 @@ export default function DashboardLayout({ children }) {
   // Navigation items data
   const navigationItems = [
     { icon: HomeIcon, label: "Home", href: "/" },
+    { icon: ClockIcon, label: "Recent", href: "/recent" },
     { icon: UserIcon, label: "Leads", href: "/leads" },
-    // { icon: ClipboardDocumentListIcon, label: "Agent Skills", href: "/agent-skills" },
     { icon: CogIcon, label: 'Settings', href: "/settings"},
   ];
 
@@ -52,26 +52,27 @@ export default function DashboardLayout({ children }) {
 
             <div className="flex flex-grow ">
               <div
-                className={`bg-gray-800 text-white transition-all duration-300 ${
+                className={`bg-gray-200 text-white transition-all duration-300 ${
                   isSidebarMinimized ? 'w-16' : 'w-40'
                 } hidden lg:flex flex-col`}
               >
                 <button
                   onClick={() => setSidebarMinimized(!isSidebarMinimized)}
-                  className="pl-5 hover:bg-gray-700"
+                  className="flex hover:bg-gray-300 justify-center mt-2"
                 >
                   <Bars3Icon
-                    className={`h-6 w-6 text-gray-500 transform transition-transform ${
+                    className={`h-6 w-6 text-slate-500 transform transition-transform ${
                       isSidebarMinimized ? 'rotate-180' : ''
                     }`}
                   />
                 </button>
+            
                 <div className="flex flex-col items-center mt-4">
                 {navigationItems.map(({ icon: Icon, label, href }, index) => (
                   <Link
                     key={index}
                     href={href}
-                    className={`flex items-center p-4 hover:bg-gray-700 cursor-pointer ${
+                    className={`flex text-slate-800 items-center p-4 hover:bg-gray-300 cursor-pointer ${
                       isSidebarMinimized ? "justify-center" : ""
                     }`}
                   >
@@ -80,13 +81,13 @@ export default function DashboardLayout({ children }) {
                   </Link>
                 ))}
                 <div
-                    className={`flex items-center p-4 hover:bg-gray-700 cursor-pointer ${
+                    className={`flex text-slate-800 items-center p-4 hover:bg-gray-300 cursor-pointer ${
                       isSidebarMinimized ? "justify-center" : ""
                     }`}
-                    onClick={() => setSkillsModalOpen(true)} // Open modal on click
+                    onClick={() => setSkillsModalOpen(true)}
                   >
                     <ClipboardDocumentListIcon className="w-6 h-6" />
-                    {!isSidebarMinimized && <span className="ml-4">Agent Skills</span>}
+                    {!isSidebarMinimized && <span className="ml-1">Agent Skills</span>}
                 </div>
               </div>
               </div>
