4afec02f1acf041f9dde4cafd977f1533bdaa15b
toyman640 <falakoadebayor@gmail.com>
2024-12-23 21:13:40 +0100

    Fix width of header card

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar() {
 
     <Disclosure as="nav" className="bg-gray-800">
       <div className="mx-auto max-w-10xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
+        <div className="relative flex text-sm h-10 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
             <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -29,7 +29,7 @@ export default function Navbar() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
-              <Squares2X2Icon className="h-6 w-6 text-white" />
+              <Squares2X2Icon className="h-6 w-6 text-white text-sm" />
             </div>
             <div className="hidden sm:ml-6 sm:block text-white">
               <div className="flex space-x-4">
--- src/app/leads/lead-component/lead-list.js
+++ src/app/leads/lead-component/lead-list.js
@@ -59,10 +59,10 @@ export default function LeadCard() {
     >
       {/* Header Section */}
       <div className="flex flex-col md:flex-row justify-between">
-        <h2 className="text-sm font-bold md:basis-2/2 text-center md:text-left">
+        <h2 className="text-sm font-bold md:basis-1/2 text-center md:text-left">
           Hi Mona, 68% of goal achieved and rest can be achieved by focusing on 20 top leads
         </h2>
-        <div className="md:mt-0 md:basis-2/2 flex flex-col items-center md:items-end lg:items-start">
+        <div className="md:mt-0 md:basis-1/2 flex flex-col items-center md:items-end lg:items-start">
           <div className="flex pb-1">
             <ClockIcon className="h-4 w-4 text-gray-500 mr-1" />
             <p className="text-xs text-gray-400">1 month until Q4 ends</p>
